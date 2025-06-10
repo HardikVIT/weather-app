b8cc6e20e0ecf972a398ee43171c22bd399b55ec
HardikVIT <Hardik.sondhi2022@vitstudent.ac.in>
2025-06-10 17:15:47 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { Parser } = require('json2csv');
 require('dotenv').config();
-const serverless = require('serverless-http');
 
 const City = require('./models/City'); // Your Mongoose model for City
 
@@ -30,7 +29,6 @@ app.get('/api/weather/export/csv', async (req, res) => {
     res.status(500).send('Failed to export CSV');
   }
 });
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -79,6 +77,5 @@ app.delete('/api/weather/:name', async (req, res) => {
   }
 });
 
-// Start server
-module.exports = serverless(app);
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
